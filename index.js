4b997fb3e5758525953e11301d089a73eb6dac90
Jaime Kumar <jaimelacree@gmail.com>
2024-10-30 14:40:55 +0000

    debug dynamic resize

--- index.js
+++ index.js
@@ -422,12 +422,21 @@ function handleResize()
     const centralContH = document.getElementById("centralCont").getBoundingClientRect().height;
 
     const totalH = paddingTop + paddingBot + logoH + kbH + centralContH + 40;
+
+    console.log("padT: " + paddingTop)
+    console.log("padB: " + paddingBot)
+    console.log("logoH: " + logoH)
+    console.log("kbH: " + kbH)
+    console.log("contH: " + centralContH)
+    console.log("totalH: " + totalH)
+    console.log("visViewPortH: " + window.visualViewport.height)
+    console.log("screenavailH: " + screen.availHeight)
     
     var tableW = document.getElementById("gameTable").getBoundingClientRect().width;
     
-    if (totalH > window.visualViewport.height)
+    if (totalH > screen.availHeight)
     {
-        tableW = tableW - (totalH - window.visualViewport.height);
+        tableW = tableW - (totalH - screen.availHeight);
         document.getElementById("gameTable").style.width = tableW+'px';
     }
     document.getElementById("gameTable").style.height = tableW+'px';
