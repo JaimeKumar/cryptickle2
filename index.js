637c50b6168be72f1e24a9501a5d1a42923e6e20
Jaime Kumar <jaimelacree@gmail.com>
2025-05-04 14:04:18 +0100

    fetch test 8

--- index.js
+++ index.js
@@ -111,7 +111,7 @@ function selectCell(cell)
 
 function calcLine()
 {
-    console.log(thisPuzzle, selectedCell, lineId)
+    console.log(thisPuzzle.cells, selectedCell, thisPuzzle.cells[selectedCell].lineID)
     if (thisPuzzle.cells[selectedCell].lineID.length < 2)
     {
         selectedLine = thisPuzzle.cells[selectedCell].lineID[0];
